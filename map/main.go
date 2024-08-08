af65add326c451f73e7b60b4563203de367bb740
sattarkarashi <satokarashi@gmail.com>
2024-08-08 18:38:15 +0200

    deleting items. using maps standard library and iteration over maps

--- map/main.go
+++ map/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"maps"
 )
 
 func main() {
@@ -23,6 +24,31 @@ func main() {
 
 	// if we try to get a value that doesn't exist it will return false and create a key with initial value
 	v2, status2 := my_map["saya"]
-	fmt.Printf("v1: %d and status is:%v ", v2, status2)
+	fmt.Printf("v1: %d and status is:%v \n", v2, status2)
+
+	// delete items
+	delete(my_map, "sati")
+	fmt.Printf("%v", my_map)
+
+	// clear all the map
+	clear(my_map)
+	fmt.Printf("%v \n", my_map)
+
+	// using maps standard library
+	my_map_2 := map[string]int{"x": 4, "y": 8}
+
+	if maps.Equal(my_map, my_map_2) {
+		fmt.Println("Equal")
+	} else {
+		fmt.Println("Not equal")
+	}
+
+	// iteration over maps doesn't guarantee the order
+	for k, v := range my_map_2 {
+		fmt.Printf("%s -> %d \n", k, v)
+	}
+
+	// in GO like other languages maps are being hashed using hash functions. keys go through the hash function which lets the access to variable faster.
+	// Giving size to maps will make processing maps faster.
 
 }
